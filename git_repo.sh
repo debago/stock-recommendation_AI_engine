616e11e8e8a10c0a5a426b1c17f87c4f596a2ca1
Debasish <debasishg2003@gmail.com>
2026-03-10 20:23:32 +0530

    added file

---
+++ git_repo.sh
@@ -0,0 +1,7 @@
+echo "# stock-recommendation_AI_engine" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/debago/stock-recommendation_AI_engine.git
+git push -u origin main
